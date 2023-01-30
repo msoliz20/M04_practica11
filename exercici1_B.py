6a3894c9ea555c1a227397f8d8c6501bd7137033
Your Name <you@example.com>
2023-01-30 20:46:44 +0100

    Subido  practica11

---
+++ exercici1_B.py
@@ -0,0 +1,10 @@
+def compara_números():
+    num1 = int(input("Introduïu el primer número: "))
+    num2 = int(input("Introduïu el segon número: "))
+
+    if num1 > num2:
+        print("El primer número és més gran")
+    elif num2 > num1:
+        print("El segon nombre és més gran")
+    else:
+        pass
---
+++ exercici2_B.py
@@ -0,0 +1,7 @@
+def check_parity():
+    num = int(input("Introduïu un número: "))
+
+    if num % 2 == 0:
+        print("El nombre és parell")
+    else:
+        print("El nombre és senar")
